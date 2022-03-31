b859f0ebcfabb83e1de7d146c812c6f0c0365702
Serge <sy.caravan@gmail.com>
2022-03-31 13:04:51 +0300

    upld

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editssss123 <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          <li></li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
